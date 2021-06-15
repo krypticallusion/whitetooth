286c6571d5da99f3a7d37395049b12123e5bf8c7
krypticallusion <krypticallusion@gmail.com>
2021-06-15 14:08:12 +0530

    Drop usage of GnomeBluetooth
    Signed-off-by: krypticallusion <krypticallusion@gmail.com>

--- extension.js
+++ extension.js
@@ -1,12 +1,11 @@
 const { St } = imports.gi;
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
-const GnomeBluetooth = imports.gi.GnomeBluetooth;
 const GLib = imports.gi.GLib;
 const BluetoothMenu = Main.panel.statusArea.aggregateMenu._bluetooth._item.menu;
 const BluetoothMenuBox = BluetoothMenu.box;
 
-let panelButtonText = new PopupMenu.PopupMenuItem(BluetoothMenu.focusActor.label.get_text() === "Bluetooth On" ? "Turn on" : "Turn off");
+let panelButtonText = new PopupMenu.PopupMenuItem(isBluetoothOn() ? "Turn on" : "Turn off");
 let originalWidget = BluetoothMenuBox.first_child;
 
 function init() {}
@@ -23,39 +22,26 @@ function spawn(command, callback) {
   if (callback) GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, callback);
 }
 
-function _getDefaultAdapter() {
-    const client = new GnomeBluetooth.Client();
-    const model = client.get_model();
-
-    let [ret, iter] = model.get_iter_first();
-    while (ret) {
-        let isDefault = model.get_value(iter,
-                                              GnomeBluetooth.Column.DEFAULT);
-        let isPowered = model.get_value(iter,
-                                              GnomeBluetooth.Column.POWERED);
-        if (isDefault && isPowered)
-            return iter;
-        ret = model.iter_next(iter);
-    }
-    return null;
+function isBluetoothOn() {
+  return !(BluetoothMenu.focusActor.label.get_text() === "Bluetooth Off");
 }
 
 function enable() {
   BluetoothMenuBox.replace_child(originalWidget, panelButtonText);
   panelButtonText.connect("activate", function (w) {
-    const adapter = _getDefaultAdapter();
-  
-    if (adapter === null) {
-        spawn("bluetoothctl power off");
+    const isBtOn = isBluetoothOn();
+
+    w.label.set_text(isBtOn ? "Turn on" : "Turn off");
+
+    if (isBtOn) {
+      spawn("bluetoothctl power off");
     } else {
-        spawn("bluetoothctl power on")
+      spawn("bluetoothctl power on");
     }
 
     // BluetoothMenu.focusActor.label.get_text() returns the previous state before the toggle.
     // Since BT is off, get_text() would show Bluetooth Off, but (adapter !==  null) so Bt would power on.
-    // Hence the label name should turn to Turn off. 
-    w.label.set_text(BluetoothMenu.focusActor.label.get_text() === "Bluetooth Off" ? "Turn off" : "Turn on")
-    
+    // Hence the label name should turn to Turn off.
   });
 }
 
